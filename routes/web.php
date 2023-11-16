2760c86bc7aac8478a3260475728133ffcd7fd9e
mokapot <nganhtu04597@gmail.com>
2023-11-16 21:14:59 +0700

    Minor fixes

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
